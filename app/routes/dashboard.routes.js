bb12b78f0813a50c69fca3b820d755741ecdce76
BananaDog <145420584+BananaBoyIsMe@users.noreply.github.com>
2023-12-01 11:22:46 +0700

    Update dashboard.routes.js

--- app/routes/dashboard.routes.js
+++ app/routes/dashboard.routes.js
@@ -2,6 +2,6 @@ module.exports = (app) => {
   const dashboard_controller = require("../controllers/dashboard.controller");
   var router = require("express").Router();
   router.get("/", dashboard_controller.getDash1);
-  router.get("/:mORp/:limit", dashboard_controller.getDash2);
+  router.get("/:choose/:id", dashboard_controller.getDash2);
   app.use("/api/dash", router);
 };
