45090f9d5837007b1ab40b699bdc5890cc5e0155
BananaDog <145420584+BananaBoyIsMe@users.noreply.github.com>
2023-12-01 11:19:50 +0700

    Update dashboard.model.js

--- app/models/dashboard.model.js
+++ app/models/dashboard.model.js
@@ -1,7 +1,7 @@
 const sql = require("./db");
 
-const topOne = (limit, result) => {
-  sql.query("SELECT merch_id, name, rating, img, url FROM merchandise ORDER BY rating DESC LIMIT " + limit + "", (err, res) => {
+const allRatingAttract = (result) => {
+  sql.query("SELECT MAX(r.rating) AS 'max_attract', AVG(r.rating) AS 'avg_attract', MIN(r.rating) AS 'min_attract', COUNT(r.rating) AS 'count_attract' FROM ratings r INNER JOIN attractions a ON r.att_id = a.id WHERE att_id IS NOT NULL", (err, res) => {
     if (err) {
       console.log("Query error:", err);
       result(err, null);
@@ -11,8 +11,8 @@ const topOne = (limit, result) => {
   });
 }
 
-const topTwo = (limit, result) => {
-  sql.query("SELECT at_id, name, image_url, rating, url FROM `attractions` ORDER BY rating DESC LIMIT " + limit + " ", (err, res) => {
+const allRatingMerch = (result) => {
+  sql.query("SELECT MAX(r.rating) AS 'max_merch', AVG(r.rating) AS 'avg_merch', MIN(r.rating) AS 'min_merch', COUNT(r.rating) AS 'count_merch' FROM ratings r INNER JOIN merchandises m ON r.merch_id = m.id WHERE merch_id IS NOT NULL", (err, res) => {
     if (err) {
       console.log("Query error:", err);
       result(err, null);
@@ -22,8 +22,63 @@ const topTwo = (limit, result) => {
   });
 }
 
-const numUser = (result) => {
-  sql.query("SELECT COUNT(id) AS 'Total number of user' FROM `users` WHERE 1", (err, res) => {
+const oneRatingAttract = (id, result) => {
+  sql.query("SELECT MAX(r.rating) AS 'max_attract', AVG(r.rating) AS 'avg_attract', MIN(r.rating) AS 'min_attract', COUNT(r.rating) AS 'count_attract' FROM ratings r INNER JOIN attractions a ON r.att_id = a.id WHERE att_id = " + id, (err, res) => {
+    if (err) {
+      console.log("Query error:", err);
+      result(err, null);
+      return;
+    }
+    result(null, res);
+  });
+}
+
+const oneRatingMerch = (id, result) => {
+  sql.query("SELECT MAX(r.rating) AS 'max_merch', AVG(r.rating) AS 'avg_merch', MIN(r.rating) AS 'min_merch', COUNT(r.rating) AS 'count_merch' FROM ratings r INNER JOIN merchandises m ON r.merch_id = m.id WHERE merch_id = " + id, (err, res) => {
+    if (err) {
+      console.log("Query error:", err);
+      result(err, null);
+      return;
+    }
+    result(null, res);
+  });
+}
+
+const topRatingAttract = (result) => {
+  sql.query("SELECT AVG(r.rating) AS rating, COUNT(r.rating) AS num_rating, a.id AS att_id FROM ratings r INNER JOIN attractions a ON r.att_id = a.id GROUP BY a.id ORDER BY rating DESC", (err, res) => {
+    if (err) {
+      console.log("Query error:", err);
+      result(err, null);
+      return;
+    }
+    result(null, res);
+  });
+}
+
+const topRatingMerch = (result) => {
+  sql.query("SELECT AVG(r.rating) AS rating, COUNT(r.rating) AS num_rating, m.id AS merch_id FROM ratings r INNER JOIN merchandises m ON r.merch_id = m.id GROUP BY m.id ORDER BY rating DESC", (err, res) => {
+    if (err) {
+      console.log("Query error:", err);
+      result(err, null);
+      return;
+    }
+    result(null, res);
+  });
+}
+
+const topThree = (limit, result) => {
+  sql.query("SELECT merch_id, name, rating, img, url FROM merchandises ORDER BY rating DESC LIMIT " + limit + "", (err, res) => {
+    if (err) {
+      console.log("Query error:", err);
+      result(err, null);
+      return;
+    }
+    result(null, res);
+  });
+}
+
+const everything = (result) => {
+  sql.query("SELECT COUNT(id) AS 'num_users' FROM `users` WHERE 1", (err, res) => {
     if (err) {
       console.log("Query error:", err);
       result(err, null);
@@ -34,7 +89,14 @@ const numUser = (result) => {
 }
 
 module.exports = {
-  topOne,
-  topTwo,
-  numUser
+  allRatingAttract,
+  allRatingMerch,
+  oneRatingAttract,
+  oneRatingMerch,
+
+  topRatingAttract,
+  topRatingMerch,
+
+  topThree,
+  everything
 };
